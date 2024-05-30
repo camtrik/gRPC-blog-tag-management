b0c4490273542fd22447d3607405171cfa4a3210
camtrik <camtrikcsky@gmail.com>
2024-05-31 01:00:55 +0900

    test with client

---
+++ client/client.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/camtrik/gRPC-blog-tag-management/proto"
+)
+
+func main() {
+	ctx := context.Background()
+	clientConn, _ := GetClientConn("localhost:8080", []grpc.DialOption{grpc.WithBlock()})
+	defer clientConn.Close()
+
+	targetServiceClient := pb.NewTagServiceClient(clientConn)
+	resp, _ := targetServiceClient.GetTagList(ctx, &pb.GetTagListRequest{Name: "go"})
+
+	log.Printf("resp: %v", resp)
+}
+
+func GetClientConn(target string, opts []grpc.DialOption) (*grpc.ClientConn, error) {
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	return grpc.NewClient(target, opts...)
+}
